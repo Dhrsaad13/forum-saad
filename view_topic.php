a40094c1533fed2a112da856b0bde2e1b0825e72
Dhrsaad13 <dhrsaad13@gmail.com>
2016-07-28 11:43:14 +0200

    Update view_topic.php

--- view_topic.php
+++ view_topic.php
@@ -1,16 +1,16 @@
 <?php
 
-$host="localhost"; // Host name 
-$username="root"; // Mysql username 
-$password=""; // Mysql password 
-$db_name="forum"; // Database name 
-$tbl_name="fquestions"; // Table name 
+$host="localhost";  
+$username="root"; 
+$password=""; 
+$db_name="forum"; 
+$tbl_name="fquestions";  
 
-// Connect to server and select databse.
+
 mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
 mysql_select_db("$db_name")or die("cannot select DB");
 
-// get value of id that sent from address bar 
+ 
 $id=$_GET['id'];
 $sql="SELECT * FROM $tbl_name WHERE id='$id'";
 $result=mysql_query($sql);
@@ -88,14 +88,14 @@ $result3=mysql_query($sql3);
 $rows=mysql_fetch_array($result3);
 $view=$rows['view'];
 
-// if have no counter value set counter = 1
+
 if(empty($view)){
 $view=1;
 $sql4="INSERT INTO $tbl_name(view) VALUES('$view') WHERE id='$id'";
 $result4=mysql_query($sql4);
 }
 
-// count more value
+
 $addview=$view+1;
 $sql5="update $tbl_name set view='$addview' WHERE id='$id'";
 $result5=mysql_query($sql5);
